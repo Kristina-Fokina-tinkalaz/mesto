6c8c560a8a5dec9b3c811fbf4e5489b05b4e1eb5
Kristina Fokina <tinkalaz@gmail.com>
2022-05-18 12:36:56 +0300

    refactor: Исправлена ошибка в JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 
 const popup = document.querySelector(".popup");
 const profile = document.querySelector(".profile");
-const profile__name = profile.querySelector(".profile__name");
-const profile__discription = profile.querySelector(".profile__discription");
-const profile__editbutton = profile.querySelector(".profile__editbutton");
+const profileName = profile.querySelector(".profile__name");
+const profileDiscription = profile.querySelector(".profile__discription");
+const profileEditbutton = profile.querySelector(".profile__editbutton");
 const formElement = document.querySelector(".edit-form");
 const nameInput = formElement.querySelector("#name");
 const jobInput = formElement.querySelector("#description");
@@ -11,24 +11,22 @@ const closeIcon = document.querySelector(".close-icon");
 
 function openPopup() {
     popup.classList.add("popup_opened");
-    nameInput.value = profile__name.textContent;
-    jobInput.value = profile__discription.textContent;
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileDiscription.textContent;
 }
 
 function closePopup() {
-    // profile__name.textContent = profile__name.textContent;
-    // profile__discription.textContent = profile__discription.textContent;
     popup.classList.remove("popup_opened");
 }
 
 function formSubmitHandler(evt) {
     evt.preventDefault(); 
-    profile__name.textContent = nameInput.value;
+    profileName.textContent = nameInput.value;
     console.log('text');
-    profile__discription.textContent = jobInput.value;
+    profileDiscription.textContent = jobInput.value;
     closePopup();
 }
 
-profile__editbutton.addEventListener("click", openPopup);
+profileEditbutton.addEventListener("click", openPopup);
 closeIcon.addEventListener("click", closePopup);
 formElement.addEventListener("submit", formSubmitHandler);
